eb9f044d446abbbc435af1e5b01df90f99b9c3e1
miriampc <miiriampc@gmail.com>
2017-03-05 11:52:05 -0500

    primer commit en rama, implementando funcion calcula peso

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+document.getElementById('calcula').onclick= function calcularPeso(){
+  var name=document.getElementById('name').value;
+  var peso=document.getElementById('peso').value;
+  peso=(peso/9.78);
+
+  alert("Hola "+name+" tu peso en estos planetas es:\n"
+        +"Marte: "+3.71*peso+"\nMercurio: "+3.72*peso+"\nVenus: "+8.82*peso);
+}
+document.getElementById('form').onsubmit = function(e){
+  e.preventDefault();
+}
