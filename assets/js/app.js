d5e687b137e37add35d1c5251f1f22df184db863
lesashley <lesashley56@gmail.com>
2017-03-05 21:28:47 -0500

    Insertando fondo y reduciendo decimales

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,19 +5,21 @@ var masa = document.getElementById('masa');
 
 calcula.onclick = function (){
 
-
   if (masa.value.length == 0 ){
     alert("Por favor completar su masa en Kg.");
   } else if (masa.value.length != 0 ){
   print.innerHTML = "<h3>Su peso es:</h3><ul><li>Mercurio: " + (0.39 * masa.value) +
-                    "</li><li>Venus: " + (0.87 * masa.value) + "</li><li>Tierra: " + (1 * masa.value) +
-                    "</li><li>Marte: " + (0.38 * masa.value) + "</li><li>Júpiter: " + (2.55 * masa.value) +
-                    "</li><li>Saturno: " + (0.93 * masa.value) + "</li><li>Urano: " + (0.99 * masa.value) +
-                    "</li><li>Neptuno: " + (1.38 * masa.value) + "</li><li>Luna: " + (0,17 * masa.value) +
+                    "</li><li>Venus: " + (0.87 * masa.value).toFixed(2) + "</li><li>Tierra: " + (1 * masa.value).toFixed(2) +
+                    "</li><li>Marte: " + (0.38 * masa.value).toFixed(2) + "</li><li>Júpiter: " + (2.55 * masa.value).toFixed(2) +
+                    "</li><li>Saturno: " + (0.93 * masa.value).toFixed(2) + "</li><li>Urano: " + (0.99 * masa.value).toFixed(2) +
+                    "</li><li>Neptuno: " + (1.38 * masa.value).toFixed(2) + "</li><li>Luna: " + (0,17 * masa.value).toFixed(2) +
                     "</li></ul>";
   }
 }
 
+document.getElementById("form").onsubmit=function(e){
+  e.preventDefault();
+}
 
 /*Mercurio = 0.39
 Venus  = 0.87
